a85977b0413c04b01a29e0000e4750eb813d2c68
Cristina Barbieri <daeima87@gmail.com>
2021-11-10 16:44:32 +0100

    Yelp MVP done, but no styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+
+category = %w[chinese italian japanese french belgian]
+
+10.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number_with_country_code,
+    category: category.sample
+  )
+  puts "Created #{restaurant.name}"
+end
+
+puts 'Finished'
